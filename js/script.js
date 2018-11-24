bb80747d50e7afa65b9559d9d5affe006c97f169
zjw93615 <zjw93615@163.com>
2018-11-24 22:32:32 +1100

    Daily Update

--- js/script.js
+++ js/script.js
@@ -83,8 +83,22 @@ $(function() {
   });
 });
 
+/* ----------------------------------
+    Testimonials
+  ----------------------------------- */
 
-
+$(function(){
+  $("#testimonial-slider").owlCarousel({
+    items: 1,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots:false,
+    navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+  });
+});
 
 
 
